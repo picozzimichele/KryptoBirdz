495006bc6c2f8b1b325a2656658fc9fc78b5675b
Michele Picozzi <michele.picozzi@hotmail.it>
2021-12-02 15:50:54 +0000

    first commit

---
+++ src/contracts/Migrations.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+  address public owner = msg.sender;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address); //We instantiate a new contract with the new address as a variable
+    upgraded.setCompleted(last_completed_migration);
+  }
+}
---
+++ truffle-config.js
@@ -0,0 +1,38 @@
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+
+    contracts_dir: "./src/contracts",
+    contracts_build_directory: "./src/abis",
+
+  },
+
+  compilers: {
+    solc: {
+      version: "^0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200 //default value
+       },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+};
