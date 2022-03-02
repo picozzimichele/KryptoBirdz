80a639d6e9dfcff4f471d4d115fc87d210e2d480
Michele Picozzi <michele.picozzi@hotmail.it>
2022-03-02 12:51:54 +0100

    finalized logic and hooked front end

---
+++ src/components/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import KryptoBird from "../abis/KryptoBird.json";
+
+class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  //detect eth provider
+  async loadWeb3() {
+    const provider = await detectEthereumProvider();
+
+    if(provider) {
+      console.log("eth wallet is connected")
+      window.web3 = new Web3(provider)
+    } else {
+      console.log("no ethereum wallet detected")
+    }
+
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account:accounts[0]})
+    console.log(this.state.account)
+
+    //create a constant js networkId which is set to the blockchain network Id
+    const networkId = await web3.eth.net.getId();
+    const networkData = KryptoBird.networks[networkId]
+    if(networkData) {
+      const abi = KryptoBird.abi
+      const address = networkData.address
+      let contract = new web3.eth.Contract(abi, address)
+
+      this.setState({contract: contract})
+      //getting the total supply from the KryptoBird contract
+      let totalSupply = await contract.methods.totalSupply().call();
+      this.setState({totalSupply})
+      for(let i = 0; i < totalSupply; i++) {
+        const KryptoBird = await contract.methods.kryptoBirdz(i).call()
+        this.setState({
+          kryptoBirdz:[...this.state.kryptoBirdz, KryptoBird]
+        })
+      }
+    } else {
+      window.alert("Smart contract not deployed")
+    }
+  }
+
+  //creating the mint function from the contract in the state and we fire the function only once
+
+  mint = (KryptoBird) => {
+    this.state.contract.methods.mint(KryptoBird).send({from: this.state.account})
+    .once('receipt', (receipt) => {
+      this.setState({
+        kryptoBirdz:[...this.state.kryptoBirdz, KryptoBird]
+      })
+    })
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      contract: "",
+      totalSupply: 0,
+      kryptoBirdz: [],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>NFT MARKETPLACE</h1>
+        <p>{this.state.account}</p>
+
+        <form onSubmit={(e) =>{
+          e.preventDefault();
+          const kryptoBird = this.kryptoBird.value
+          this.mint(kryptoBird)
+        }}>
+            <input 
+              type="text" 
+              placeholder='Add file Location' 
+              ref={(input) => this.kryptoBird = input}
+            />
+            <input type='submit' value="MINT"/>
+        </form>
+
+        {/* Display NFTs */}
+        {this.state.kryptoBirdz.map((NFT, index) => {
+          return(<p key={index}>{NFT}</p>)
+        })}
+      </div>
+    )
+  }
+}
+
+export default App; 
--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 // eslint-disable-next-line no-undef
-const KryptoBird = artifacts.require("Kryptobird");
+const KryptoBird = artifacts.require("KryptoBird");
 
 //check for chai
 require('chai')
@@ -8,13 +8,19 @@ require('chai')
     .should()
 
 // eslint-disable-next-line no-undef
-contract('KryptoBird', accounts => {
+contract('KryptoBird', (accounts) => {
     let contract
+
+    //before runs first every test
+    // eslint-disable-next-line no-undef
+    before( async() => {
+        contract= await KryptoBird.deployed()
+    })
+    
 
     describe('deployment', async() => {
         
         it('deploys successfully', async() => {
-            contract = await KryptoBird.deployed()
             const address = contract.address;
             assert.notEqual(address, '');
             assert.notEqual(address, null);
@@ -23,15 +29,51 @@ contract('KryptoBird', accounts => {
         })
 
         it('name of the contract matches', async() => {
-            contract = await KryptoBird.deployed()
             const contractName = await contract.name();
             assert.equal(contractName, 'KryptoBird');
         })
 
         it('name of the symbol matches', async() => {
-            contract = await KryptoBird.deployed()  
             const contractSymbol = await contract.symbol();
             assert.equal(contractSymbol, 'KBIRDZ');
         })
+    })
+
+    describe('minting', async() => {
+        it("creates a new token", async() => {
+            const result = await contract.mint('https...1')
+            const totalSupply = await contract.totalSupply()
+            
+            //success
+            assert.equal(totalSupply, 1)
+            const event = result.logs[0].args
+            assert.equal(event._from, '0x0000000000000000000000000000000000000000', 'from is the contract')
+            assert.equal(event._to, accounts[0], 'to is msg.sender')
+
+            //failure
+            await contract.mint('https...1').should.be.rejected;
+        })
+    })
+
+    describe('indexing', async() => {
+        it('lists KryptoBirdz', async() => {
+            await contract.mint('https...2')
+            await contract.mint('https...3')
+            await contract.mint('https...4')
+            const totalSupply = await contract.totalSupply()
+
+            //loop through list and grab KBirdz from list
+            let result = []
+            let KryptoBird
+
+            for(let i = 0; i < totalSupply; i++) {
+                KryptoBird = await contract.kryptoBirdz(i)
+                result.push(KryptoBird)
+            }
+            //compare the arrays
+            let expected = ['https...1', 'https...2', 'https...3', 'https...4']
+            //we can compare 2 strings made from the arrays
+            assert.equal(expected.join(','), result.join(','))
+        })  
     })
 });
